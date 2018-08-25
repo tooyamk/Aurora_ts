a63e1c0cabf3f59076aed49a4c70b8b5f24d965d
tooyamk <0xff000000@gmail.com>
2018-08-25 19:18:16 +0800

    null

--- scripts/src/_Tester.ts
+++ scripts/src/_Tester.ts
@@ -4,9 +4,5 @@
 /// <reference path="physics/Ray.ts" />
 /// <reference path="math/MathUtils.ts" />
 
-let ray = new MITOIA.Ray();
-let hit = ray.castPlane(new MITOIA.Vector3(1, 1, 10), new MITOIA.Vector3(0, 0, 1));
-let v = MITOIA.MathUtils.getFootOfPerpendicular(MITOIA.Vector3.Zero, new MITOIA.Vector3(1, 0, 0), new MITOIA.Vector3(-20, 100, 7));
-
 //new _Other();
 new _SimpleWorld();
